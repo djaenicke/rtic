4b0bf4a49350899f0a2ae2234abaa586cdb1f5fb
Devin K. Jaenicke <djaenicke.92@gmail.com>
2022-04-17 11:48:30 -0600

    added the ability to use C++

--- firmware/src/board/board.h
+++ firmware/src/board/board.h
@@ -6,12 +6,15 @@
 #include "io.h"
 #include "stm32f4xx_hal.h"
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
 void initBoard(void);
-
 void serialTxString(const char* const string);
-
 void toggleHeartbeat(void);
-
 void delayMs(const uint32_t delay_ms);
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // BOARD_H
