e56f27136c2214180d704bdbf3a30b04a1058fc4
Devin K. Jaenicke <djaenicke.92@gmail.com>
2022-07-02 23:01:37 -0600

    added nrf24 driver

---
+++ firmware/src/nrf24_hal.h
@@ -0,0 +1,11 @@
+#ifndef NRF_24_HAL_H
+#define NRF_24_HAL_H
+
+#include <stdint.h>
+
+#define nRF24_CSN_L  // todo
+#define nRF24_CSN_H  // todo
+
+uint8_t nRF24_LL_RW(uint8_t data);
+
+#endif  // NRF_24_HAL_H
